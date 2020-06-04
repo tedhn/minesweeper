e043c696d5ff19a85aff3ae6bf472f0df1a7e717
ted <heinhtetnaing186@gmail.com>
2020-06-05 03:43:34 +0630

    Updates

---
+++ precache-manifest.27e4d43c6af8d33fb8ab498e443051de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0e46375bd4f8d8c536c580a6577c35",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "9acb28b81c13e1c10c38",
+    "url": "/minesweeper/static/css/main.ecdd97e2.chunk.css"
+  },
+  {
+    "revision": "5b483640154c864fd3be",
+    "url": "/minesweeper/static/js/2.37df030a.chunk.js"
+  },
+  {
+    "revision": "cb8ae04c59de3e3bdb56c06f5e9f08d8",
+    "url": "/minesweeper/static/js/2.37df030a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9acb28b81c13e1c10c38",
+    "url": "/minesweeper/static/js/main.5fa9dad7.chunk.js"
+  },
+  {
+    "revision": "fa48b2abd22ffe206c98",
+    "url": "/minesweeper/static/js/runtime-main.eb128309.js"
+  }
+]);
